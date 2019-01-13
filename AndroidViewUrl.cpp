649badd7a216ae9f5df13ca097d8f568ea063ca7
NiloofarRezaei <nrezaei95@yahoo.com>
2019-01-13 10:06:44 +0330

    first commit

---
+++ AndroidViewUrl.cpp
@@ -0,0 +1,6 @@
+#include "AndroidViewUrl.h"
+
+AndroidViewUrl::AndroidViewUrl()
+{
+
+}
---
+++ AndroidViewUrl.h
@@ -0,0 +1,11 @@
+#ifndef ANDROIDVIEWURL_H
+#define ANDROIDVIEWURL_H
+
+
+class AndroidViewUrl
+{
+public:
+    AndroidViewUrl();
+};
+
+#endif // ANDROIDVIEWURL_H
